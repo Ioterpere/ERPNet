96a96d7262f02e1699a24f3fb887405fb57dd3ea
Ioterpere <iago.otero7@gmail.com>
2026-02-11 14:04:29 +0100

    Entidad Nullable en LogResponse

--- ERPNet.Application/DTOs/LogResponse.cs
+++ ERPNet.Application/DTOs/LogResponse.cs
@@ -5,7 +5,7 @@ public class LogResponse
     public long Id { get; set; }
     public int? UsuarioId { get; set; }
     public string Accion { get; set; } = null!;
-    public string Entidad { get; set; } = null!;
+    public string? Entidad { get; set; }
     public string? EntidadId { get; set; }
     public DateTime Fecha { get; set; }
     public string? Detalle { get; set; }
